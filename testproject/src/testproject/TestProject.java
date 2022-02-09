e8b5b0db6f39123be74e9b5db1bcd300b586a203
Shreeya Waghmare <shreeyawaghmare4@gmail.com>
2022-02-09 23:47:40 +0530

    first commit

---
+++ testproject/src/testproject/TestProject.java
@@ -0,0 +1,10 @@
+package testproject;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my java project!");
+	}
+
+}
